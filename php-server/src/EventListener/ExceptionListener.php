f9d7af539986205dff64e3d92742c16e2f4e6e92
Faisal Ibrahim <faisal.im048@gmail.com>
2023-04-12 07:57:18 +0600

    Log un-handled exceptions to log file

--- php-server/src/EventListener/ExceptionListener.php
+++ php-server/src/EventListener/ExceptionListener.php
@@ -4,13 +4,23 @@ namespace App\EventListener;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Psr\Log\LoggerInterface;
 
 class ExceptionListener
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function onKernelException(ExceptionEvent $event)
     {
         // Get the exception object from the event
         $exception = $event->getThrowable();
+
+        //Log non http exception
+        if (empty($exception->getCode()) || $exception->getCode() == 0) {
+            $this->logger->error($exception->getMessage());
+        }
 
         // Create the JSON response
         $response = new JsonResponse([
