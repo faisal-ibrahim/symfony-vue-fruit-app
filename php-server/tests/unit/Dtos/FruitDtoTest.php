65345075a5d377ea314393e464e2dc02a2e38501
Faisal Ibrahim <faisal.im048@gmail.com>
2023-04-13 02:00:20 +0200

    Added unit test for Dtos

---
+++ php-server/tests/unit/Dtos/FruitDtoTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\unit\Dtos;
+
+use PHPUnit\Framework\TestCase;
+use App\Dtos\FruitDto;
+
+final class FruitDtoTest extends TestCase
+{
+    public function testGetters()
+    {
+        $fruitDto = new FruitDto(
+            'Apple',
+            'Rosaceae',
+            'Malus',
+            1,
+            'Rosales',
+            52.1,
+            0.2,
+            10.3,
+            13.8,
+            0.3
+        );
+
+        $this->assertEquals('Apple', $fruitDto->getName());
+        $this->assertEquals('Rosaceae', $fruitDto->getFamily());
+        $this->assertEquals('Malus', $fruitDto->getGenus());
+        $this->assertEquals(1, $fruitDto->getFruityviceId());
+        $this->assertEquals('Rosales', $fruitDto->getFruitOrder());
+        $this->assertEquals(52.1, $fruitDto->getCalories());
+        $this->assertEquals(0.2, $fruitDto->getFat());
+        $this->assertEquals(10.3, $fruitDto->getSugar());
+        $this->assertEquals(13.8, $fruitDto->getCarbohydrates());
+        $this->assertEquals(0.3, $fruitDto->getProtien());
+    }
+}
